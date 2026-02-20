405a57429cd87ebe8da335d63175b31874c0f629
Stavros <132775095+Stavros-alt@users.noreply.github.com>
2026-02-19 21:53:06 -0500

    fix: naming fix

--- ut_song_data.js
+++ ut_song_data.js
@@ -881,7 +881,7 @@ const utSongList = [
     },
     {
         "id": 1110,
-        "name": "Good Night",
+        "name": "Mus pianofix",
         "file": "UNDERTALEsoundtrack/toby fox - UNDERTALE Soundtrack - 110 Good Night.mp3",
         "rating": 1500,
         "comparisons": 0,
